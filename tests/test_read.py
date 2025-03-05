68ed09467f53a11c8d6f0bfa6e6e7d8c62501b60
Anjali Ratnam <anjaliratnam@microsoft.com>
2025-03-05 09:35:34 -0800

    test updates

--- tests/test_read.py
+++ tests/test_read.py
@@ -15,27 +15,56 @@ from azure.identity import DefaultAzureCredential
 from azstoragetorch.io import BlobIO
 from azstoragetorch._client import AzStorageTorchBlobClient
 
+from azure.core.exceptions import ResourceExistsError
+from azure.storage.blob import BlobServiceClient
 
-EXPECTED_DEFAULT_READLINE_PREFETCH_SIZE = 4 * 1024 * 1024
-
-
-@pytest.fixture
-def sas_token():
-    return "sp=r&st=2024-10-28T20:22:30Z&se=2024-10-29T04:22:30Z&spr=https&sv=2022-11-02&sr=c&sig=signature"
 
 
 @pytest.fixture
-def blob():
-    return BlobIO(
-        blob_url="https://anjaliratnamtest.blob.core.windows.net/test/test.txt",
+def account_url():
+    account_name = os.environ.get('PYTORCH_STORAGE_ACCOUNT_NAME', None) or "pytorchstorageaccount"
+    return "https://" + account_name.strip('"') + ".blob.core.windows.net"
+
+@pytest.fixture
+def container_name():
+    return ''.join(random.choices(string.ascii_lowercase + string.digits, k=8))
+
+@pytest.fixture
+def blob_name():
+    return ''.join(random.choices(string.ascii_lowercase + string.digits, k=8))
+
+@pytest.fixture
+def sample_data():
+    return ''.join(random.choices(string.ascii_lowercase + string.digits, k=8)).encode()
+
+@pytest.fixture
+def container(
+    account_url, container_name
+):
+    blob_service_client = BlobServiceClient(account_url, credential=DefaultAzureCredential())
+
+    cont = blob_service_client.create_container(name=container_name)
+    yield blob_service_client
+    cont.delete_container()
+
+@pytest.fixture
+def blob(
+    account_url, container_name, blob_name, sample_data, container
+):  
+    blob_client = container.get_blob_client(container=container_name, blob=blob_name)
+    blob_client.upload_blob(sample_data, blob_type="BlockBlob")
+
+    yield BlobIO(
+        blob_url=account_url + "/" + container_name + "/" + blob_name,
         mode="rb"
     )
 
+
 class TestRead:
     def test_reads_all_data(
-        self, blob
+        self, blob, sample_data
     ):
         with blob as f:
-            assert f.read() == b"Hello World"
+            assert f.read() == sample_data
 
     
