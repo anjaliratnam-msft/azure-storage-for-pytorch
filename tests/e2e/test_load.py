525cfde03c66acbe318caac6367d76f3fb8aff1f
Anjali Ratnam <anjaliratnam@microsoft.com>
2025-03-24 14:45:30 -0700

    test updates

--- tests/e2e/test_load.py
+++ tests/e2e/test_load.py
@@ -5,53 +5,42 @@
 # --------------------------------------------------------------------------
 import pytest
 import torch
-import ssl
-import functools
-import certifi
 
 from azstoragetorch.io import BlobIO
 
 
-@pytest.fixture(scope="module")
-def ssl_inject_certifi(monkeypatch):
-    # Sets the global variable for the SSL certificates file path since torch.hub doesn't use the certifi package
-    monkeypatch.setattr(
-        ssl,
-        "_create_default_https_context",
-        functools.partial(ssl._create_default_https_context, cafile=certifi.where()),
-    )
+@pytest.fixture(scope="module", autouse=True)
+def torch_hub_cache(tmp_path_factory):
+    current_dir = torch.hub.get_dir()
+    yield tmp_path_factory.mktemp("torch_hub")
+    torch.hub.set_dir(current_dir)
 
 
 @pytest.fixture(scope="module")
-def tmp_path(tmp_path_factory):
-    return tmp_path_factory.mktemp("torch_hub")
-
-
-@pytest.fixture(scope="module")
-def model(tmp_path):
-    torch.hub.set_dir(tmp_path)
+def model(torch_hub_cache):
+    torch.hub.set_dir(torch_hub_cache)
     model = torch.hub.load("pytorch/vision:v0.10.0", "resnet101", pretrained=False)
     return model
 
 
 @pytest.fixture(scope="module")
-def upload_model(model, container_client):
-    blob_name = "model.pth"
+def upload_model(model, container_client, torch_hub_cache):
+    blob_name = torch_hub_cache / "model.pth"
     torch.save(model.state_dict(), blob_name)
-    blob_client = container_client.get_blob_client(blob=blob_name)
+    blob_client = container_client.get_blob_client(blob=blob_name.name)
     with open(blob_name, "rb") as f:
         blob_client.upload_blob(f)
-    return blob_name
+    return blob_name.name
 
 
 @pytest.fixture()
-def blob_url(account_url, container_client, upload_model):
+def state_dict_blob_url(account_url, container_client, upload_model):
     return f"{account_url}/{container_client.container_name}/{upload_model}"
 
 
 class TestLoad:
-    def test_load_existing_model(self, blob_url, model):
-        with BlobIO(blob_url, "rb") as f:
+    def test_load_existing_model(self, state_dict_blob_url, model):
+        with BlobIO(state_dict_blob_url, "rb") as f:
             state_dict = torch.load(f)
 
         assert state_dict.keys() == model.state_dict().keys()
