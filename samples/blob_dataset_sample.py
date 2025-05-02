98557c0168a91140f6ce2ccf13912a5cca92ccf3
Anjali Ratnam <anjaliratnam@microsoft.com>
2025-05-02 13:32:57 -0700

    updates

--- samples/blob_dataset_sample.py
+++ samples/blob_dataset_sample.py
@@ -1,22 +1,21 @@
 from azstoragetorch.datasets import BlobDataset
 
-
-def create_dataset_from_container_url(CONTAINER_URL, prefix=None):
-    if prefix is None:
-        map_dataset = BlobDataset.from_container_url(CONTAINER_URL)
-    else:
-        map_dataset = BlobDataset.from_container_url(CONTAINER_URL, prefix=prefix)
-
-def main(account_name, container_name):
+def create_dataset_from_container_url(my_storage_account_name, my_container_name):
     # Update URL with your own Azure Storage account and container name
-    CONTAINER_URL = "https://<my-storage-account-name>.blob.core.windows.net/<my-container-name>"
+    CONTAINER_URL = f"https://{my_storage_account_name}.blob.core.windows.net/{my_container_name}"
 
     # Create a map-style dataset by listing blobs in the container specified by CONTAINER_URL.
     map_dataset = BlobDataset.from_container_url(CONTAINER_URL)
 
+
+def create_dataset_from_container_url_with_prefix(my_storage_account_name, my_container_name, prefix):
+    # Update URL with your own Azure Storage account and container name
+    CONTAINER_URL = f"https://{my_storage_account_name}.blob.core.windows.net/{my_container_name}"
+
     # Create a map-style dataset only including blobs whose name starts with the prefix "images/"
     map_dataset = BlobDataset.from_container_url(CONTAINER_URL, prefix="images/")
 
+def create_dataset_from_blob_urls(CONTAINER_URL):
     # List of blob URLs to create dataset from. Update with your own blob names.
     blob_urls = [
         f"{CONTAINER_URL}/<blob-name-1>",
@@ -29,6 +28,9 @@ def main(account_name, container_name):
 
 
 if __name__ == "__main__":
-    account_name = "<my-storage-account-name>"
-    container_name = "<my-container-name>"
-    main(account_name, container_name)
+    account_name = "anjaliratnamtest"
+    container_name = "test"
+    create_dataset_from_container_url(account_name, container_name)
+
+    # Create a map-style dataset only including blobs whose name starts with the prefix "images/"
+    # create_dataset_from_container_url_with_prefix(account_name, container_name, "images/")
--- samples/iterable_blob_dataset_sample.py
+++ samples/iterable_blob_dataset_sample.py
@@ -1,20 +1,27 @@
 from azstoragetorch.datasets import IterableBlobDataset
 
-# Update URL with your own Azure Storage account and container name
-CONTAINER_URL = "https://<my-storage-account-name>.blob.core.windows.net/<my-container-name>"    
 
-# Create an iterable-style dataset by listing blobs in the container specified by CONTAINER_URL.
-iterable_dataset = IterableBlobDataset.from_container_url(CONTAINER_URL)
 
-# Create an iterable-style dataset only including blobs whose name starts with the prefix "images/"
-iterable_dataset = IterableBlobDataset.from_container_url(CONTAINER_URL, prefix="images/")
+def main():
+    # Update URL with your own Azure Storage account and container name
+    CONTAINER_URL = "https://<my-storage-account-name>.blob.core.windows.net/<my-container-name>"    
 
-# List of blob URLs to create dataset from. Update with your own blob names.
-blob_urls = [
-    f"{CONTAINER_URL}/<blob-name-1>",
-    f"{CONTAINER_URL}/<blob-name-2>",
-    f"{CONTAINER_URL}/<blob-name-3>",
-]
+    # Create an iterable-style dataset by listing blobs in the container specified by CONTAINER_URL.
+    iterable_dataset = IterableBlobDataset.from_container_url(CONTAINER_URL)
 
-# Create an iterable-style dataset from the list of blob URLs
-iterable_dataset = IterableBlobDataset.from_blob_urls(blob_urls)
+    # Create an iterable-style dataset only including blobs whose name starts with the prefix "images/"
+    iterable_dataset = IterableBlobDataset.from_container_url(CONTAINER_URL, prefix="images/")
+
+    # List of blob URLs to create dataset from. Update with your own blob names.
+    blob_urls = [
+        f"{CONTAINER_URL}/<blob-name-1>",
+        f"{CONTAINER_URL}/<blob-name-2>",
+        f"{CONTAINER_URL}/<blob-name-3>",
+    ]
+
+    # Create an iterable-style dataset from the list of blob URLs
+    iterable_dataset = IterableBlobDataset.from_blob_urls(blob_urls)
+
+
+if __name__ == "__main__":
+    main()
