559a06ffdad8a42f263b8429e2100c6925438952
Anjali Ratnam <anjaliratnam@microsoft.com>
2025-05-05 09:09:56 -0700

    updates

--- samples/blob_dataset_sample.py
+++ samples/blob_dataset_sample.py
@@ -1,4 +1,20 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+
+"""
+FILE: blob_dataset_sample.py
+DESCRIPTION:
+    This sample demonstrates the ways to create a map-style dataset from Azure Blob Storage using the BlobDataset class.
+USAGE: python blob_dataset_sample.py
+    Set the environment variables with your own values before running the sample.
+
+"""
+
 from azstoragetorch.datasets import BlobDataset
+
 
 def create_dataset_from_container_url(my_storage_account_name, my_container_name):
     # Update URL with your own Azure Storage account and container name
@@ -6,6 +22,7 @@ def create_dataset_from_container_url(my_storage_account_name, my_container_name
 
     # Create a map-style dataset by listing blobs in the container specified by CONTAINER_URL.
     map_dataset = BlobDataset.from_container_url(CONTAINER_URL)
+    print(map_dataset[0])
 
 
 def create_dataset_from_container_url_with_prefix(my_storage_account_name, my_container_name, prefix):
@@ -15,7 +32,11 @@ def create_dataset_from_container_url_with_prefix(my_storage_account_name, my_co
     # Create a map-style dataset only including blobs whose name starts with the prefix "images/"
     map_dataset = BlobDataset.from_container_url(CONTAINER_URL, prefix="images/")
 
-def create_dataset_from_blob_urls(CONTAINER_URL):
+
+def create_dataset_from_blob_urls():
+     # Update URL with your own Azure Storage account and container name
+    CONTAINER_URL = f"https://{my_storage_account_name}.blob.core.windows.net/{my_container_name}"
+    
     # List of blob URLs to create dataset from. Update with your own blob names.
     blob_urls = [
         f"{CONTAINER_URL}/<blob-name-1>",
@@ -33,4 +54,6 @@ if __name__ == "__main__":
     create_dataset_from_container_url(account_name, container_name)
 
     # Create a map-style dataset only including blobs whose name starts with the prefix "images/"
-    # create_dataset_from_container_url_with_prefix(account_name, container_name, "images/")
+    create_dataset_from_container_url_with_prefix(account_name, container_name, "images/")
+
+    create_dataset_from_blob_urls()
